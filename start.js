26be495f4df3375074b14c71f2f8414f1c49af02
color-quest <co-st.transport541@passinbox.com>
2025-02-21 23:08:46 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,7 +1,23 @@
-const { Deepstream } = require('@deepstream/server')
+const { Deepstream } = require('@deepstream/server');
+
+// Get default port
+const port = process.env.WS_PORT || 3000;
 
 // Create server
-const server = new Deepstream(null)
-
+const server = new Deepstream(
+{
+  connectionEndpoints:
+  [
+    {
+      type: 'ws-websocket',
+      options:
+      {
+        port: port,
+        host: '0.0.0.0'
+      }
+    }
+  ]
+});
+      
 // start the server
-server.start()
+server.start();
