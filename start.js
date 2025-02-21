1fb8b412e1c8e215d31096e2fa5e1480e357d593
color-quest <co-st.transport541@passinbox.com>
2025-02-21 23:33:55 +0100

    Update start.js

--- start.js
+++ start.js
@@ -6,15 +6,16 @@ const port = process.env.WS_PORT || 3000;
 // Create server
 const server = new Deepstream(
 {
+  httpServer:
+  {
+    type: 'default',
+    options:
+    {
+        port: port
+    }
+  },
   connectionEndpoints:
   [
-    {
-      type: 'http',
-      options:
-      {
-        port: port
-      }
-    },
     {
       type: 'ws-binary',
       options:
